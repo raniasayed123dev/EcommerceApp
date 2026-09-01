da111c10b4013801c4b0d8f96cbe43eb7f7df8da
raniasayed123dev <raniasayedzaki@gmail.com>
2026-09-01 14:51:55 +0300

    Initial Commit

---
+++ EcommerceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcommerceApp
+//
+//  Created by rania on 01/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by rania on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
